7c988722a28d8d1084d811d0b5258a6116117f69
Vinnie Chen <vinniechen@vinnies-macbook-pro-2.local>
2016-11-22 02:51:59 -0800

    Initial Commit

---
+++ FridgeApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FridgeApp
+//
+//  Created by Vinnie Chen on 11/22/16.
+//  Copyright © 2016 Vinnie Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
