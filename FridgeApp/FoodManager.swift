92a1900047d1cc5e782f82adbb78e6561b5688d5
Vinnie Chen <vinniechen@vinnies-macbook-pro-2.local>
2016-12-15 16:32:28 -0500

    Main page displays days until expiration

--- FridgeApp/FoodManager.swift
+++ FridgeApp/FoodManager.swift
@@ -10,17 +10,86 @@ import UIKit
 
 // Global Scope
 var foodMgr: FoodManager = FoodManager() // creates instance of FoodManager
+let foodSort = NSSortDescriptor(key: "date", ascending: true, selector: #selector(NSString.localizedStandardCompare(_:))) // sort by date
 
 struct foodItem {
     var name = "No_Name"
-    var date = "No_Date"
+    var expDate: Date?
+
     
 }
 
 class FoodManager: NSObject {
-
-    var foods = [foodItem]();
+    
+    var foods = NSMutableOrderedSet();
+    
     func addFood(name: String, date: String) {
-        foods.append(foodItem(name: name, date: date))
+        
+        let expDate = setExpirationDate(food: name) // get the expiration date
+        foods.add(foodItem(name: name, expDate: expDate))
+        //foods.sort(using: [foodSort]) // sort by date
+    }
+    
+    // Assign expDate in foodItem
+    func setExpirationDate(food: String) -> Date {
+        // Get shelf life of food
+        let shelfLife = 10 // currently constant val, will change to pull from database
+        
+        let addDate = Date()
+        let cal = NSCalendar(identifier: NSCalendar.Identifier.gregorian)
+        let expirationDate = cal?.date(byAdding: NSCalendar.Unit.day, value: shelfLife, to: addDate, options: [])
+        return expirationDate!
+    }
+    
+    // Calculate Int days until Expiration for food
+    func getDaysToExp(expDate: Date) -> Int {
+        let cal = NSCalendar(identifier: NSCalendar.Identifier.gregorian)
+        let currDate = Date()
+        
+        let date1 = cal?.startOfDay(for: expDate)
+        let date2 = cal?.startOfDay(for: currDate)
+        
+        let flags = NSCalendar.Unit.day
+        let dayDifference = cal?.components(flags, from: date1!, to: date2!, options: [])
+      return abs(dayDifference!.day!)
+    }
+    
+    // Format string to be used in TableView
+    func formatForView(totalDays: Int) -> String {
+        if (totalDays == 0) { return "Expires Today" }
+        
+        // calculate the weeks and the days
+        let days = totalDays % 7
+        let weeks = totalDays/7
+        
+        var formattedString = ""
+        
+        if (weeks > 0) {
+            if (days == 1) {
+                formattedString += "\(weeks) week "
+            }
+            else {
+                formattedString += "\(weeks) weeks "
+            }
+        }
+        if (days > 0) {
+            
+            if (days == 1) {
+                formattedString += "\(days) day "
+            }
+            else {
+                formattedString += "\(days) days "
+            }
+        }
+        
+        return formattedString
+    }
+    
+    func calculateDate() -> String {
+        let NSdate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let date = dateFormatter.string(from: NSdate)
+        return date
     }
 }
